a87698a48369443ba9a75d058a62b16b44cd1c2e
Boomertaters <boomeritaintaters@gmail.com>
2026-07-23 22:53:19 -0400

    Add normal mark to subcategory

--- lua/entities/terminator_nextbot_markinator/shared.lua
+++ lua/entities/terminator_nextbot_markinator/shared.lua
@@ -5,17 +5,9 @@ ENT.Base = "terminator_nextbot_markinator_plier"
 DEFINE_BASECLASS( ENT.Base )
 ENT.PrintName = "Mark"
 ENT.Spawnable = false
-list.Set( "NPC", "terminator_nextbot_markinator", {
-    Name = "Mark",
-    Class = "terminator_nextbot_markinator",
-    Category = "Terminator Nextbot",
-} )
+terminator_Extras.RegisterNPC( "terminator_nextbot_markinator", ENT )
 
-if CLIENT then
-    language.Add( "terminator_nextbot_markinator", ENT.PrintName )
-    return
-
-end
+if CLIENT then return end
 
 ENT.IsFodder = true
 ENT.CoroutineThresh = terminator_Extras.baseCoroutineThresh / 1.5
@@ -30,9 +22,8 @@ ENT.AccelerationSpeed = 1500
 ENT.JumpHeight = 70 * 1.5
 ENT.FistDamageMul = 0.45
 ENT.ThrowingForceMul = 0.5
-ENT.SpawnHealth = 210
+ENT.SpawnHealth = 100
 ENT.MyPhysicsMass = 150
 
 
 ENT.FootstepClomping = false
-
